d3f0dff1f84c173a595be64a5d0ce3c628b4e5b1
VsIG <Valentyn2202@gmail.com>
2020-05-18 22:37:36 +0300

    Revert "made a progress"
    This reverts commit 39e1248d3b017306e10ecd2405eb14db583315b6.

--- Program.cs
+++ Program.cs
@@ -28,18 +28,21 @@ namespace Console_Game_CSharp
             //[10,14] is for grid and [2,2] is for border
 
             MakingMatrix(tetrisGrid);
+            printBoundary(tetrisGrid, 12, 16);
 
             SetShape();
             currentShape = nextShape;
-            Array.Copy(currentShape, currentShape.GetLowerBound(0), tetrisGrid, 7, 1);//instead of 3 create variable,
-            //that will hold length of block
+                Array.Copy(currentShape, currentShape.GetLowerBound(0), tetrisGrid, 7, 2);
             System.Threading.Thread.Sleep(1000);
-            Console.WriteLine(tetrisGrid.Length);
-            SearchingForBlocks(tetrisGrid);
-            //MoveBlockDown();
+            MoveBlockDown();
+            // var result = Filter(tetrisGrid, u => u[0] == 3);
+            //Console.WriteLine(result);
 
             Array.Copy(currentShape, currentShape.GetLowerBound(0), tetrisGrid, 7, 2);
-
+            //var result = Filter(tetrisGrid, u => u[0] == 3);
+            //Console.WriteLine(result);
+            tetrisGrid.GetValue()
+            //Console.WriteLine(tetrisGrid[11, 7]);
             MakingMatrix(tetrisGrid);
 
             for (int i = 0; i < currentShape.GetLength(0); i++)
@@ -49,19 +52,6 @@ namespace Console_Game_CSharp
                     Console.Write(currentShape[i, j]);
                 }
                 Console.WriteLine();
-            }
-            printBoundary(tetrisGrid, 12, 16);
-        }
-
-        public static void SearchingForBlocks(int[,] tetrisGrid)
-        {
-            foreach (int i in tetrisGrid)
-            {
-                if (i == 3)//you can make some stopper(count elements in block and,if this
-                    //function has found all of 'em-break)
-                {
-                    Console.WriteLine("YOU ARE AMAZING");
-                }
             }
         }
 
