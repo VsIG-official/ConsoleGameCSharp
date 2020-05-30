0a12c164bea4f98ed58db125c74fbc15490f6042
VsIG <Valentyn2202@gmail.com>
2020-05-30 19:28:21 +0300

    Create UnitTest1.cs

---
+++ TetrisConsoleGameCSharp/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TetrisConsoleGameCSharpTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
